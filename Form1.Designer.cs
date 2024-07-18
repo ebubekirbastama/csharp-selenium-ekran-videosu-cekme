0564be06ea4df61e9f77a8e59d1d46fcb4d3bbfc
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2024-07-18 22:46:54 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Csharp_Selenium_Ekrankayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Teste Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "monster bilgisayar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(480, 191);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EBS Tester Ekran Kayıt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace Csharp_Selenium_Ekrankayit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string url = "https://ebubekirbastama.com.tr/EBSScreenVideoSave.html";
+        string url1 = "https://www.trendyol.com/";
+        string searchxpath = "//*[@placeholder='Aradığınız ürün, kategori veya markayı yazınız']";
+        ChromeDriver drv; Thread th;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(kayitbasla); th.Start();
+        }
+        private void kayitbasla()
+        {
+            EkranKayitBaslat();
+
+            #region Trendyol Test
+            drv.SwitchTo().Window(drv.WindowHandles[1]);
+            drv.FindElement(By.XPath(searchxpath)).SendKeys(textBox1.Text + OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#search-app > div > div.srch-rslt-cntnt > div.srch-prdcts-cntnr.srch-prdcts-cntnr-V2.search-products-container-for-blacklistUrl > div:nth-child(2) > div > div > div > button.quick-filters-item.collectableCoupon\").click();");
+            Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#search-app > div > div.srch-rslt-cntnt > div.srch-prdcts-cntnr.srch-prdcts-cntnr-V2.search-products-container-for-blacklistUrl > div:nth-child(2) > div > div > div > button.quick-filters-item.rushDeliveryTimes\").click();");
+            Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#search-app > div > div.srch-rslt-cntnt > div.srch-prdcts-cntnr.srch-prdcts-cntnr-V2.search-products-container-for-blacklistUrl > div:nth-child(2) > div > div > div > button.quick-filters-item.freeCargo\").click();");
+            drv.ExecuteScript("document.querySelector(\"#sticky-aggregations > div > div:nth-child(2) > div.fltrs > div > div > div:nth-child(1) > div > a\").click();"); Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#sticky-aggregations > div > div:nth-child(2) > div.fltrs > div > div > div:nth-child(3) > div > a\").click();"); Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#sticky-aggregations > div > div:nth-child(2) > div.fltrs > div > div > div:nth-child(1) > div > a\").click();"); Thread.Sleep(3000);
+            drv.ExecuteScript("document.querySelector(\"#sticky-aggregations > div > div:nth-child(2) > div.fltrs > div > div > div:nth-child(3) > div > a\").click();"); Thread.Sleep(3000);
+            drv.SwitchTo().Window(drv.WindowHandles[0]);
+            drv.FindElement(By.XPath("//*[@id='stopBtn']")).Click();
+            MessageBox.Show("Test Bitti");
+            #endregion
+        }
+
+        private void EkranKayitBaslat()
+        {
+            ChromeOptions op = new ChromeOptions();
+            op.AddArguments("use-fake-ui-for-media");
+            drv = new ChromeDriver(op);
+            drv.Navigate().GoToUrl(url);
+            drv.ExecuteScript($"window.open('{url1}');");
+            drv.SwitchTo().Window(drv.WindowHandles[0]);
+            drv.FindElement(By.XPath("//*[@id='startBtn']")).Click();
+            Thread.Sleep(3000);
+
+                SendKey("{TAB}");
+                Thread.Sleep(100); // Tuşlar arasında gecikme
+                SendKey("{TAB}");
+                Thread.Sleep(100); // Tuşlar arasında gecikme
+                SendKey("{DOWN}");
+                Thread.Sleep(100); // Tuşlar arasında gecikme
+                SendKey("{TAB}");
+                Thread.Sleep(100); // Tuşlar arasında gecikme
+                SendKey(" ");
+
+            
+        }
+
+        static void SendKey(string key)
+        {
+            SendKeys.SendWait(key);
+        }
+
+    }
+}
